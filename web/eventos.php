5d70992b79132fb7fa457c1c00e83be84cbed3e7
Jonas Abreu <jonas@vidageek.net>
2012-11-04 16:38:30 -0200

    removendo evento duplicado

--- web/eventos.php
+++ web/eventos.php
@@ -17,13 +17,6 @@
 			</div>
 			<p>A Arteccom, atenta &agrave;s &uacute;ltimas mudan&ccedil;as do mercado, traz para o Edted todo o conte&uacute;do do evento FNEC – F&oacute;rum Nacional de E-commerce –, que foi um sucesso em suas quatro edi&ccedil;&otilde;es. <br /> Para acompanhar a velocidade desse mercado e esclarecer os constantes questionamentos desse p&uacute;blico – ag&ecirc;ncias e lojistas, o EDTED E-commerce trar&aacute; grandes cases inovadores. Al&eacute;m de absorver esse conte&uacute;do exclusivo, voc&ecirc; ainda estar&aacute; em contato com os especialistas mais experientes no assunto em um ambiente prop&iacute;cio &agrave;s parcerias e troca de ideias.</p>
 		</section>
-		<section class="events">
-			<h3>Agile Brasil 2012</h3>
-			<div>
-				<img src="http://www.agilebrazil.com/2012/imagens/logo_2012_h.png" alt="" />
-			</div>
-			<p>A Agile Brazil &eacute; a mais relevante confer&ecirc;ncia brasileira sobre M&eacute;todos &Aacute;geis de desenvolvimento de software.</p>
-		</section>
 	</section>
 </div>
 
